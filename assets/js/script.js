6e40f2551a9326d6580035fdc1eb74ef7e653691
Vaibhav-BRS <krishvaibu@gmail.com>
2024-04-02 12:14:20 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,31 @@ function setTheme(theme) {
   localStorage.setItem("movie-theme", theme);
 }
 setTheme(localStorage.getItem("movie-theme") || chathams_blue);
+
+// Get the container where buttons will be appended
+const container = document.getElementById('container');
+
+// Define the characters for the buttons
+const characters = [
+  ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Delete'],
+  ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']'],
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'Shift'],
+  ['Space']
+];
+
+// Create buttons
+characters.forEach(rowChars => {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  
+  rowChars.forEach(char => {
+    const button = document.createElement('button');
+    button.classList.add('keys', 'btn');
+    button.textContent = char;
+    row.appendChild(button);
+  });
+  
+  container.appendChild(row);
+});
